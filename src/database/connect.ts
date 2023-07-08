0e1e8c9261c1ecedb028406d1b8e83dd0d0f844a
julio cesar <jcmcf2012@gmail.com>
2023-07-08 20:33:56 -0300

    closes #5, infra: add mongodb connection

---
+++ src/database/connect.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+// Database connection URL
+const dbUrl = process.env.DB_KEY;
+
+// Connect to the database
+const connectToMongo = () => {
+  mongoose.connect(dbUrl);
+
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to the database");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.error("Database connection error:", err);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Disconnected from the database");
+  });
+};
+
+export { connectToMongo };
