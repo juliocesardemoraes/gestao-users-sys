3b3b91d0ad1ff037ffc53ed77554edc630ce52cf
julio cesar <jcmcf2012@gmail.com>
2023-07-20 08:14:32 -0300

    feat: add user services

--- src/users/service.ts
+++ src/users/service.ts
@@ -10,14 +10,61 @@ const getUsers = async (request, response) => {
   return users;
 };
 
+const getUserById = async (request, response) => {
+  try {
+    const user = await User.findById(request);
+    if (user == null) {
+      const mongoError: any = {
+        mongoError: { errorMessage: "User Not Found", status: 404 },
+      };
+      return mongoError;
+    }
+    return user;
+  } catch (error) {
+    console.log("ERROR", error);
+    const mongoError: any = {
+      mongoError: {
+        ...error,
+        errorMessage: "Bad Request Check Your Parameter",
+        status: 400,
+      },
+    };
+    return mongoError;
+  }
+};
+
 const createUser = async (userToCreate: IUser, response) => {
   try {
     const userCreate = await User.create(userToCreate);
     return userCreate;
   } catch (error) {
-    error.error = true;
-    return error;
+    const mongoError: any = {
+      mongoError: { ...error },
+    };
+
+    if (error.code == "11000") {
+      mongoError.mongoError.errorMessage = "Email is already in use";
+    }
+
+    return mongoError;
   }
 };
 
-export { getUsers, createUser };
+const updateUser = async (userToCreate: IUser, response) => {
+  try {
+    const userCreate = await User.create(userToCreate);
+    return userCreate;
+  } catch (error) {
+    const mongoError: any = {
+      mongoError: { ...error },
+    };
+
+    if (error.code == "11000") {
+      mongoError.mongoError.errorMessage = "Email is already in use";
+    }
+
+    return mongoError;
+  }
+};
+
+export { getUsers, createUser, getUserById, updateUser };
