53af954b6562bcfb4403f788f7bf80a69406ee1c
julio cesar <jcmcf2012@gmail.com>
2023-07-20 08:27:05 -0300

    fix: change for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import mongoose from "mongoose";
-import "dotenv/config";
+import express from "express";
+import { router } from "./routes.js";
+import { connectToMongo } from "./database/connect.js";
+import { userRouter } from "./users/controller.js";
 
-const check = true;
-mongoose.connect(process.env.DB_KEY);
+// SETUP DATABASE
+connectToMongo();
+
+const app = express();
+app.use(express.json());
+
+// SETUP ROUTES
+app.use(userRouter);
+
+app.listen("3000");
